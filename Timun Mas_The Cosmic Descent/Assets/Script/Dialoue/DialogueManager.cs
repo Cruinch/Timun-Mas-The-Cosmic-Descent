20afa9af315ae7aa8348a8538c04592efd9e4cc2
Cruinch <127633115+Cruinch@users.noreply.github.com>
2023-12-06 11:46:58 +0700

    Add Dialogue Box, Pause Menu Button and Animation

---
+++ Timun Mas_The Cosmic Descent/Assets/Script/Dialoue/DialogueManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    public Image budi;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentences();
+    }
+
+    public void DisplayNextSentences()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+    void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+    }
+}
